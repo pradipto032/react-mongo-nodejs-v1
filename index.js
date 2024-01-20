917cba96a7fa5537ca7f576f7f97a97536713226
pradipto032 <pradipto.daw1999@gmail.com>
2024-01-21 01:34:03 +0530

    Changed the ID logic. Used UUID

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.listen(5038, ()=>{
     })
 })
 
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+          v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
 app.get("/api/todoapp/GetNotes", (req, res) => {
     database.collection("todoappcollection").find({}).toArray((error, result) => {
         res.send(result);
@@ -28,7 +36,7 @@ app.get("/api/todoapp/GetNotes", (req, res) => {
 app.post("/api/todoapp/AddNotes", multer().none(), (req, res) => {
     database.collection("todoappcollection").count({}, (error, numOfDocs) => {
         database.collection("todoappcollection").insertOne({
-            id: (numOfDocs+1).toString(),
+            id: generateUUID(),
             description: req.body.newNotes
         });
         res.json("Added successfully");
